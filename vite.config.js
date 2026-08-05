ecd1f36c2b8d0078a54feb7ccda5e99de87a68b7
pawanagar <pawannaagar698@gmail.com>
2026-08-06 02:19:21 +0530

    dep

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/Booking-calendar/",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const isGitHubPages = env.GITHUB_ACTIONS === 'true'
+
+  return {
+    plugins: [react()],
+    base: isGitHubPages ? '/booking-calendar/' : '/',
+  }
 })
