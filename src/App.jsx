1b61d13bc75ff6c6a72304e17ab0c146ae02311a
pawanagar <pawannaagar698@gmail.com>
2026-08-06 02:10:16 +0530

    new change

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 import Navbar from './components/Navbar'
 import BookingCalendar from './components/bookingcalendar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Navbar/>
-      <BookingCalendar/>
+      <Navbar />
+      <BookingCalendar />
     </>
   )
 }
